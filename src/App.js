1a1c36be8a5b689be0fe467b8f8df32dd9f05cec
Rodney Swaji <rodney.swaji@student.moringaschool.com>
2025-10-26 02:47:05 +0300

    final commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 function App() {
   return (
-    <div></div>
-  )
+    <div>
+      <h1>Select Pizza Toppings</h1>
+      <input
+        type="checkbox"
+        id="pepperoni"
+        checked={false}
+        aria-checked={false}
+      />
+      <label htmlFor="pepperoni">Add pepperoni</label>
+      <label htmlFor="pepperoni">Add pepperoni</label>
+
+<h2>Your Toppings:</h2>
+<ul>
+  <li>Cheese</li>
+</ul>
+    </div>
+  );
 }
 
 export default App;
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,3 +4,18 @@ import App from "../App";
 import '@testing-library/jest-dom';
 
 // Code tests here
+test("pizza checkbox is initially unchecked", () => {
+  render(<App />);
+
+  const addPepperoni = screen.getByRole("checkbox", { name: /add pepperoni/i });
+
+  expect(addPepperoni).not.toBeChecked();
+});
+
+test("toppings list initially contains only cheese", () => {
+  render(<App />);
+
+  expect(screen.getAllByRole("listitem").length).toBe(1);
+  expect(screen.getByText("Cheese")).toBeInTheDocument();
+  expect(screen.queryByText("Pepperoni")).not.toBeInTheDocument();
+});
